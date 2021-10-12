aa92599ce9a4d61fed1adbe4a83aa3c36bf5805c
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-12 22:52:33 +0200

    created classes

---
+++ src/main/kotlin/FacebookClasses/Page.kt
@@ -0,0 +1,15 @@
+package FacebookClasses
+
+class Page(var posts: MutableList<Post>) {
+    override fun toString() : String {
+        var result = ""
+        for(post in posts) {
+            result += "$post\n"
+        }
+        return result
+    }
+
+    fun addPost(post: Post) {
+        posts.add(post)
+    }
+}
---
+++ src/main/kotlin/FacebookClasses/PersonPrivate.kt
@@ -0,0 +1,9 @@
+package FacebookClasses
+
+class PersonPrivate (firstName: String?, lastName: String?, friends: MutableList<Person>)
+    : Person(firstName!!, lastName!!, friends) {
+
+    override fun printFriends() : String {
+        return "this account is private"
+    }
+}
