f8a642b0ac613cc3163656819113176a8e0266fd
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-27 11:24:36 +0200

    Initialised Spring

--- src/main/kotlin/FacebookClasses/PersonPrivate.kt
+++
@@ -1,9 +0,0 @@
-package FacebookClasses
-
-class PersonPrivate (firstName: String?, lastName: String?, friends: MutableList<Person>)
-    : Person(firstName!!, lastName!!, friends) {
-
-    override fun printFriends() : String {
-        return "this account is private"
-    }
-}
